c106ceab7623d765d8b58794a29a36e262d68e37
unknown <t8kope00@students.oamk.fi>
2022-11-02 15:54:21 +0200

    projekti alustettu

--- frontend/mainwindow.h
+++ frontend/mainwindow.h
@@ -1,13 +1,7 @@
 #ifndef MAINWINDOW_H
 #define MAINWINDOW_H
 
-#include "studentwindow.h"
-#include "myurl.h"
-
 #include <QMainWindow>
-#include <QtNetwork>
-#include <QNetworkAccessManager>
-#include <QJsonDocument>
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
@@ -21,16 +15,7 @@ public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
-private slots:
-    void on_btnLogin_clicked();
-    void loginSlot (QNetworkReply *reply);
-
 private:
     Ui::MainWindow *ui;
-    StudentWindow *objectStudentWindow;
-    QNetworkAccessManager *loginManager;
-    QNetworkReply *reply;
-    QByteArray response_data;
-    QString id_student;
 };
 #endif // MAINWINDOW_H
