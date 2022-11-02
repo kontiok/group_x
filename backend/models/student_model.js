19c298aea48ff12589c1c46626cc634763661da1
unknown <t8kope00@students.oamk.fi>
2022-11-02 18:55:38 +0200

    student taulun crud operaatiot toimii ja salasanan kryptaus lisätty.

--- backend/models/student_model.js
+++ backend/models/student_model.js
@@ -1,4 +1,6 @@
 const db = require('../database');
+const bcrypt = require('bcryptjs');
+const saltRounds = 10;
 
 const student = {
     getById: function (id, callback) {
@@ -8,21 +10,23 @@ const student = {
         return db.query('select * from student', callback);
     },
     add: function (add_data, callback) {
+        bcrypt.hash(add_data.password, saltRounds, function (err, hashedPassword) {
         return db.query(
             'insert into student (id_student,fname,lname,password) values(?,?,?,?)',
-            [add_data.id_student, add_data.fname, add_data.lname, add_data.password],
-            callback
-        );
+            [add_data.id_student, add_data.fname, add_data.lname, hashedPassword],
+            callback);
+        });
     },
     delete: function (id, callback) {
         return db.query('delete from student where id_student=?', [id], callback);
     },
     update: function (id, update_data, callback) {
-        return db.query(
-            'update student set fname=?,lname=?, password=? where id_student=?',
-            [update_data.fname, update_data.lname, update_data.password, id],
-            callback
-        );
+        bcrypt.hash(update_data.password, saltRounds, function (err, hashedPassword) {
+            return db.query( 
+                'update student set fname=?,lname=?, password=? where id_student=?',
+                [update_data.fname, update_data.lname, hashedPassword, id],
+                callback);
+            });
         
     }
 };
